b0ed213fc57277a9a570b212a7462b13436f6e8a
devsarahgeo <devsarahgeo@gmail.com>
2026-01-16 13:58:58 -0500

    video metrics updated

--- dbt-dag/dags/youtube_analytics/models/fct/fct_video_metrics.sql
+++ dbt-dag/dags/youtube_analytics/models/fct/fct_video_metrics.sql
@@ -2,9 +2,8 @@ with stg as (
     select
         video_id,
         channel_id,
-        video_published_at as published_at,
-        LPAD(COALESCE(TO_NUMBER(REGEXP_SUBSTR(duration, '(\d+)S')),0),2,'0') AS readable_duration,
-        length(description) as description_length
+        video_published_at as published_at
+        -- length(description) as description_length
     from {{ ref('stg_youtube_videos') }}
 )
 
